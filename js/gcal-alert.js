424e3aee6754bd9677c08291072be0534baa7836
Brian <bychan@gmail.com>
2017-10-28 23:48:30 -0700

    Can add messages to the internal queue.

--- js/gcal-alert.js
+++ js/gcal-alert.js
@@ -1,10 +1,25 @@
 
-var currentScript = document.currentScript;
+let currentScript = document.currentScript;
+let gCalAlertModal = undefined;
+let notificationAudio = currentScript.dataset.notificationAudio;
 
-function gentleAlert(msg) {
-	audioNotification = currentScript.dataset.audioNotification;
-	console.log('hi: ' + msg);
-
+function GCalAlertModal() {
+	this.queue = [];
+	this.modalDomNode = undefined;
 }
 
-window.alert = gentleAlert;
+GCalAlertModal.prototype.queueMsg = function(msg) {
+	this.queue.push(msg);
+	console.log('queueMsg::queue:' + this.queue);
+}
+
+function gCalAlert(msg) {
+	audioNotification = currentScript.dataset.audioNotification;
+	console.log('msg: ' + msg);
+	if (gCalAlertModal === undefined) {
+		gCalAlertModal = new GCalAlertModal();
+	}
+	gCalAlertModal.queueMsg(msg);
+}
+
+window.alert = gCalAlert;
